df9c5cefa1ed8f12977d57bcba9b34d619093428
petarkosic <avt3914@gmail.com>
2024-01-18 23:22:35 +0100

    add: prometheus and grafana
    - Add prometheus and grafana to the project.
    - Export metrics from users and posts services.

--- posts/src/index.ts
+++ posts/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import os from 'os';
 import morgan from 'morgan';
+import { register, collectDefaultMetrics, Counter } from 'prom-client';
 
 const app = express();
 const port = process.env.PORT || 3002;
@@ -8,12 +9,33 @@ const port = process.env.PORT || 3002;
 app.use(express.json());
 app.use(morgan('tiny'));
 
+collectDefaultMetrics();
+
+const requestCounter = new Counter({
+	name: 'posts_request_count',
+	help: 'Total number of requests to the posts service',
+	labelNames: ['endpoint'],
+});
+
 app.get('/posts', (req, res) => {
 	const containerID = os.hostname();
 	res.header('X-Container-ID', containerID);
 	res.send(
 		`Hello, Posts! This request was handled by Container ${containerID}`
 	);
+
+	requestCounter.inc({ endpoint: '/posts' });
+});
+
+app.get('/metrics', async (req, res) => {
+	try {
+		const metrics = await register.metrics();
+		res.set('Content-Type', register.contentType);
+		res.end(metrics);
+	} catch (err) {
+		console.error('Error while collecting metrics', err);
+		res.status(500).end();
+	}
 });
 
 app.listen(port, () => {
--- users/src/index.ts
+++ users/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import os from 'os';
 import morgan from 'morgan';
+import { register, collectDefaultMetrics, Counter } from 'prom-client';
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -8,12 +9,33 @@ const port = process.env.PORT || 3001;
 app.use(express.json());
 app.use(morgan('tiny'));
 
+collectDefaultMetrics();
+
+const requestCounter = new Counter({
+	name: 'users_request_count',
+	help: 'Total number of requests to the users service',
+	labelNames: ['endpoint'],
+});
+
 app.get('/users', (req, res) => {
 	const containerID = os.hostname();
 	res.header('X-Container-ID', containerID);
 	res.send(
 		`Hello, Users! This request was handled by Container ${containerID}`
 	);
+
+	requestCounter.inc({ endpoint: '/users' });
+});
+
+app.get('/metrics', async (req, res) => {
+	try {
+		const metrics = await register.metrics();
+		res.set('Content-Type', register.contentType);
+		res.end(metrics);
+	} catch (err) {
+		console.error('Error while collecting metrics', err);
+		res.status(500).end();
+	}
 });
 
 app.listen(port, () => {
