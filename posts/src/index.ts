28e580d5f30a90a90ff3404ecbcbe204874feff2
petarkosic <avt3914@gmail.com>
2024-01-19 23:56:25 +0100

    add: histogram
    - Add histogram to monitor requests duration.
    - Add prometheus data volume.

--- posts/src/index.ts
+++ posts/src/index.ts
@@ -1,7 +1,12 @@
 import express from 'express';
 import os from 'os';
 import morgan from 'morgan';
-import { register, collectDefaultMetrics, Counter } from 'prom-client';
+import {
+	collectDefaultMetrics,
+	Counter,
+	Registry,
+	Histogram,
+} from 'prom-client';
 
 const app = express();
 const port = process.env.PORT || 3002;
@@ -9,28 +14,55 @@ const port = process.env.PORT || 3002;
 app.use(express.json());
 app.use(morgan('tiny'));
 
-collectDefaultMetrics();
+const registry = new Registry();
+collectDefaultMetrics({ register: registry });
 
-const requestCounter = new Counter({
-	name: 'posts_request_count',
-	help: 'Total number of requests to the posts service',
-	labelNames: ['endpoint'],
+// Create a counter for successful requests
+const successfulRequestsCounter = new Counter({
+	name: 'api_requests_successful',
+	help: 'Total number of successful API requests',
+	registers: [registry],
+});
+
+// Create a counter for failed requests
+const failedRequestsCounter = new Counter({
+	name: 'api_requests_failed',
+	help: 'Total number of failed API requests',
+	registers: [registry],
+});
+
+// Create a histogram for request durations
+const requestDurationHistogram = new Histogram({
+	name: 'api_request_duration_seconds',
+	help: 'Histogram of API request durations',
+	registers: [registry],
+	buckets: [0.1, 0.5, 1, 2, 5],
 });
 
 app.get('/posts', (req, res) => {
-	const containerID = os.hostname();
-	res.header('X-Container-ID', containerID);
-	res.send(
-		`Hello, Posts! This request was handled by Container ${containerID}`
-	);
+	const endTimer = requestDurationHistogram.startTimer();
 
-	requestCounter.inc({ endpoint: '/posts' });
+	try {
+		const containerID = os.hostname();
+		res.header('X-Container-ID', containerID);
+		res.send(
+			`Hello, Posts! This request was handled by Container ${containerID}`
+		);
+
+		successfulRequestsCounter.inc();
+	} catch (err) {
+		failedRequestsCounter.inc();
+		const error = err as Error;
+		console.error('Error while processing request', error.message);
+	} finally {
+		endTimer();
+	}
 });
 
 app.get('/metrics', async (req, res) => {
 	try {
-		const metrics = await register.metrics();
-		res.set('Content-Type', register.contentType);
+		const metrics = await registry.metrics();
+		res.set('Content-Type', registry.contentType);
 		res.end(metrics);
 	} catch (err) {
 		console.error('Error while collecting metrics', err);
